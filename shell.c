c0bc916c273feb0bc25cb8d6556d11926c858ab1
tlagu004.fiu <tlagu004.fiu@gmail.com>
2026-02-09 08:37:44 +0000

    Implement Support for Input and Output Redirection

--- shell.c
+++ shell.c
@@ -8,6 +8,8 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <fcntl.h>  
+#include <sys/stat.h>
 
 #define MAX_LINE 80  /* The maximum length command */
 
@@ -64,20 +66,43 @@ int main(void)
             int background = 0;
             if (strcmp(args[i-1], "&") == 0) {
                 background = 1;
-                args[i-1] = NULL; // Remove '&' so the command doesn't see it
+                args[i-1] = NULL; 
             }
             pid_t pid = fork();
             
-            if (pid < 0) { // Error
+            if (pid < 0) { 
                 fprintf(stderr, "Fork Failed");
                 return 1;
             } 
-            else if (pid == 0) { // Child Process
+            else if (pid == 0) { 
+                for (int j = 0; args[j] != NULL; j++) {
+                    if (strcmp(args[j], ">") == 0) {
+                        int fd = open(args[j+1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+                        if (fd < 0) {
+                            perror("Error opening file");
+                            exit(1);
+                        }
+                        dup2(fd, STDOUT_FILENO); 
+                        close(fd);  
+                        args[j] = NULL;
+                    }
+
+                    else if (strcmp(args[j], "<") == 0) {
+                        int fd = open(args[j+1], O_RDONLY);
+                        if (fd < 0) {
+                            perror("Error opening file");
+                            exit(1);
+                        }
+                        dup2(fd, STDIN_FILENO);
+                        close(fd);                        
+                        args[j] = NULL;
+                    }
+                }
                 execvp(args[0], args);
-                printf("Invalid Command\n"); // Only runs if execvp fails
+                perror("Exec Command\n");
                 return 1;
             } 
-            else { // Parent Process
+            else { 
                 if (background == 0) {wait(NULL);}
                 else {printf("Background process started\n");} 
             }
