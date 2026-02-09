8f90fe1e0040b412cbd41aec596e3f4247c6c59f
tlagu004.fiu <tlagu004.fiu@gmail.com>
2026-02-09 16:44:05 +0000

    Implement Communication via Pipeline

--- shell.c
+++ shell.c
@@ -11,7 +11,7 @@
 #include <fcntl.h>  
 #include <sys/stat.h>
 
-#define MAX_LINE 80  /* The maximum length command */
+#define MAX_LINE 80  
 
 int main(void)
 {
@@ -49,10 +49,13 @@ int main(void)
             }
             strcpy(input, history);
             printf("%s\n", input);
-        } else {
+        } else if (strlen(input) > 0){
+
             strcpy(history, input);
             has_history = 1;
-        }
+        } else {
+            continue;
+        } 
 
         int i = 0;
         char *token = strtok(input, " \n");
@@ -62,31 +65,70 @@ int main(void)
         }
         args[i] = NULL;
         
-        if (i > 0){
-            int background = 0;
-            if (strcmp(args[i-1], "&") == 0) {
-                background = 1;
-                args[i-1] = NULL; 
+        int pipe_idx = -1;
+        for (int j = 0; args[j] != NULL; j++) {
+            if (strcmp(args[j], "|") == 0) {
+                pipe_idx = j;
+                break;
             }
-            pid_t pid = fork();
-            
-            if (pid < 0) { 
-                fprintf(stderr, "Fork Failed");
-                return 1;
-            } 
-            else if (pid == 0) { 
-                for (int j = 0; args[j] != NULL; j++) {
-                    if (strcmp(args[j], ">") == 0) {
-                        int fd = open(args[j+1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
-                        if (fd < 0) {
-                            perror("Error opening file");
-                            exit(1);
-                        }
-                        dup2(fd, STDOUT_FILENO); 
-                        close(fd);  
-                        args[j] = NULL;
-                    }
+        }
 
+        if (pipe_idx != -1) {
+            args[pipe_idx] = NULL; 
+            char **args2 = &args[pipe_idx + 1]; 
+        
+            int fd[2];
+            if (pipe(fd) == -1) {
+                perror("Pipe failed");
+            }
+        
+            if (fork() == 0) {
+                dup2(fd[1], STDOUT_FILENO);
+                close(fd[0]); 
+                close(fd[1]);
+                execvp(args[0], args);
+                exit(1);
+            }
+        
+            if (fork() == 0) {
+                dup2(fd[0], STDIN_FILENO); 
+                close(fd[1]); 
+                close(fd[0]);
+                execvp(args2[0], args2);
+                exit(1);
+            }
+
+            close(fd[0]);
+            close(fd[1]);
+            wait(NULL);
+            wait(NULL);
+        } else {
+
+            if (i > 0){
+                int background = 0;
+                if (strcmp(args[i-1], "&") == 0) {
+                    background = 1;
+                    args[i-1] = NULL; 
+                }
+                pid_t pid = fork();
+                
+                if (pid < 0) { 
+                    fprintf(stderr, "Fork Failed");
+                    return 1;
+                } 
+                else if (pid == 0) { 
+                    for (int j = 0; args[j] != NULL; j++) {
+                        if (strcmp(args[j], ">") == 0) {
+                            int fd = open(args[j+1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+                            if (fd < 0) {
+                                perror("Error opening file");
+                                exit(1);
+                            }
+                            dup2(fd, STDOUT_FILENO); 
+                            close(fd);  
+                            args[j] = NULL;
+                        }
+                        
                     else if (strcmp(args[j], "<") == 0) {
                         int fd = open(args[j+1], O_RDONLY);
                         if (fd < 0) {
@@ -103,12 +145,16 @@ int main(void)
                 return 1;
             } 
             else { 
-                if (background == 0) {wait(NULL);}
+                if (background == 0) {
+                    waitpid(pid, NULL, 0);
+                    // wait(NULL);
+                }
                 else {printf("Background process started\n");} 
             }
         }
+        }
     }
-
+    
     printf("Program terminated\n");
     return 0;
 }
